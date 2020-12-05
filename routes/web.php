0b38197f5fc06b5bdcb6bb5fd5f0891a52ffc3a4
leokaa <linhduy8a5@gmail.com>
2020-12-05 17:45:02 +0700

    up vender

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/hello','ExampleController@xinchao');
 Route::get('/goodbye','ExampleController@tambiet');
 Route::get('/tinh','ExampleController@tinh');
+ */
